585be6338e759a256873fae38fe49090c7fccce0
David <nundavid99@gmail.com>
2023-05-07 18:25:58 -0400

    planning items class renaming & n4 implemented

---
+++ src/planning_items_classification/N4_planning_items_classification_id.py
@@ -0,0 +1,78 @@
+import sys
+sys.path.append('./src')
+
+BATCH_SIZE = 1000
+QUERY_FILE_PATH = './Query.sql'
+COUNT_MAP_FILENAME = 'saved_count_map.pkl'
+
+class N4PlanningItemsClassification:
+	def load_count_map(self, row: tuple) -> int:
+		count = 0
+		if row[self.colNumber]:
+			if('items' in row[self.colNumber]):
+				for item in row[self.colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id']
+						if id in self.count_map:
+							self.count_map[id] += 1
+						else:
+							self.count_map[id] = 1
+						count += 1
+		return count
+
+	def get_count_and_load_map(self, rows: list[tuple]) -> int:
+		total_count: int = 0
+		for row in rows:
+			total_count += self.load_count_map(row)
+		return total_count
+		
+	def load_quartiles(self, counted_map: dict, total_count: int):
+		count = 0
+		for (id, appearances_count) in counted_map.items():
+			if count <= total_count * 0.25:
+				self.first_quantile[id] = appearances_count
+			elif count <= total_count * 0.5:
+				self.second_quantile[id] = appearances_count
+			elif count <= total_count * 0.75:
+				self.third_quantile[id] = appearances_count
+			count += appearances_count
+
+	def process_row(self, row: tuple):
+		idArr = [0, 0, 0, 0]
+		if row[self.colNumber]:
+			if('items' in row[self.colNumber]):
+				for item in row[self.colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id']
+						if id in self.first_quantile:
+							idArr[0] += 1
+						elif id in self.second_quantile:
+							idArr[1] += 1
+						elif id in self.third_quantile:
+							idArr[2] += 1
+						else:
+							idArr[3] += 1
+		
+		return idArr
+
+	def print_dict(self, file_name:str, dict: dict):
+		with open(f'__out/{file_name}', 'w') as f:
+			sys.stdout = f # Change the standard output to the file we created.
+			# print('This message will be written to a file.')
+			for (a, b) in dict.items():
+				print(a, ',', b)
+			sys.stdout = sys.__stdout__ # Reset
+
+	def __init__(self, rows: list[tuple], colNumber: int):
+		self.count_map = {}
+		self.colNumber = colNumber
+		self.first_quantile: dict = {}
+		self.second_quantile = {}
+		self.third_quantile = {}	
+
+		total_count = self.get_count_and_load_map(rows)
+
+		sorted_by_count = sorted(self.count_map.items(), key=lambda x:x[1], reverse=True) #reverse True to really work
+		converted_dict = dict(sorted_by_count)
+		
+		self.load_quartiles(converted_dict, total_count)
