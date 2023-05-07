c4a13a03e633c60325a47adc9455b65960600227
David <nundavid99@gmail.com>
2023-05-07 18:42:48 -0400

    n1 & n2 planning items class id

--- src/planning_items_classification/N1_1_planning_items_classification_id.py
+++ src/planning_items_classification/N1_1_planning_items_classification_id.py
@@ -1,83 +1,63 @@
 import sys
 sys.path.append('./src')
-import helpers
 
 BATCH_SIZE = 1000
 QUERY_FILE_PATH = './Query.sql'
 COUNT_MAP_FILENAME = 'saved_count_map.pkl'
 
-count_map = {}
-index_map = {}
+class N1_1PlanningItemsClassification:
+	def load_count_map(self, row: tuple) -> int:
+		count = 0
+		if row[self.colNumber]:
+			if('items' in row[self.colNumber]):
+				for item in row[self.colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id'][0:2]
+						if id in self.count_map:
+							self.count_map[id] += 1
+						else:
+							self.count_map[id] = 1
+						count += 1
+		return count
 
-def load_count_map(row: tuple) -> int:
-	count = 0
-	if row[1]:
-		if('items' in row[1]):
-			for item in row[1]['items']:
-				if('classification' in item):
-					id = item['classification']['id'][0:2]
-					if id in count_map:
-						count_map[id] += 1
-					else:
-						count_map[id] = 1
-					count += 1
-	return count
+	def get_count_and_load_map(self, rows: list[tuple]) -> int:
+		total_count: int = 0
+		for row in rows:
+			total_count += self.load_count_map(row)
+		return total_count
+		
+	def load_indexed_map(self, counted_map: dict, ):
+		index_c = 0
+		for (id, _) in counted_map.items():
+			self.index_map[id] = index_c
+			index_c += 1
 
-def get_count_and_load_map(rows: list[tuple]) -> int:
-	total_count: int = 0
-	for row in rows:
-		total_count += load_count_map(row)
-	return total_count
-	
-def load_indexed_map(counted_map: dict, total_count: int):
-	index_c = 0
-	for (id, _) in counted_map.items():
-		index_map[id] = index_c
-		index_c += 1
+	def process_row(self, row: tuple, colNumber: int):
+		idArr = [0] *( len(list(self.index_map)) + 1 )
+		if row[colNumber]:
+			if('items' in row[colNumber]):
+				for item in row[colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id'][0:2]
+						index = self.index_map[id]
+						idArr[index] += 1
+		return idArr
 
-def process_row(row: tuple):
-	idArr = [0] *( len(list(index_map)) + 1 )
-	if row[1]:
-		if('items' in row[1]):
-			for item in row[1]['items']:
-				if('classification' in item):
-					id = item['classification']['id'][0:2]
-					index = index_map[id]
-					idArr[index] += 1
-	return idArr
+	def print_dict(self, file_name:str, dict: dict):
+		with open(f'__out/{file_name}', 'w') as f:
+			sys.stdout = f # Change the standard output to the file we created.
+			# print('This message will be written to a file.')
+			for (a, b) in dict.items():
+				print(a, ',', b)
+			sys.stdout = sys.__stdout__ # Reset
 
-def print_dict(file_name:str, dict: dict):
-	with open(f'__out/{file_name}', 'w') as f:
-		sys.stdout = f # Change the standard output to the file we created.
-		# print('This message will be written to a file.')
-		for (a, b) in dict.items():
-			print(a, ',', b)
-		sys.stdout = sys.__stdout__ # Reset
+	def __init__(self, rows: list[tuple], colNumber: int):
+		self.colNumber = colNumber
+		self.count_map = {}
+		self.index_map = {}
 
-def main(arguments):
-	query = """
-		SELECT
-			data['compiledRelease']['ocid'] as "id",
-			data['compiledRelease']['planning'] as "planning"
-		FROM RECORD r join data d on d.id = r.data_id
-	"""
-	rows = helpers.get_rows(query)
-	total_count = get_count_and_load_map(rows)
-	print_dict('counted_map.txt', count_map)
-
-	sorted_by_count = sorted(count_map.items(), key=lambda x:x[1], reverse=True) #reverse True to really work
-	converted_dict = dict(sorted_by_count)
-	
-	print_dict('sorted_counted_map.txt', converted_dict)
-	
-	load_indexed_map(converted_dict, total_count)
-
-	print_dict('all_values_map.txt', index_map)
-
-	for row in rows:
-		idArr = process_row(row)
-		print(row[0], ',', idArr)
-	
-	
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+		total_count = self.get_count_and_load_map(rows)
+		sorted_by_count = sorted(self.count_map.items(), key=lambda x:x[1], reverse=True) #reverse True to really work
+		converted_dict = dict(sorted_by_count)
+		
+		self.load_indexed_map(converted_dict)
--- src/planning_items_classification/N2_planning_items_classification_id.py
+++ src/planning_items_classification/N2_planning_items_classification_id.py
@@ -6,89 +6,70 @@ BATCH_SIZE = 1000
 QUERY_FILE_PATH = './Query.sql'
 COUNT_MAP_FILENAME = 'saved_count_map.pkl'
 
-count_map = {}
-index_map = {}
+class N2PlanningItemsClassification:
+	def load_count_map(self, row: tuple) -> int:
+		count = 0
+		if row[self.colNumber]:
+			if('items' in row[self.colNumber]):
+				for item in row[self.colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id'][0:4]
+						if id in self.count_map:
+							self.count_map[id] += 1
+						else:
+							self.count_map[id] = 1
+						count += 1
+		return count
 
-main_map_70: dict = {}
+	def get_count_and_load_map(self, rows: list[tuple]) -> int:
+		total_count: int = 0
+		for row in rows:
+			total_count += self.load_count_map(row)
+		return total_count
+		
+	def load_70(self, counted_map: dict, total_count: int):
+		count = 0
+		index_c = 0
+		for (id, appearances_count) in counted_map.items():
+			if count <= total_count * 0.7:
+				self.main_map_70[id] = index_c
+				index_c += 1
+			else:
+				break
+			count += appearances_count
 
-def load_count_map(row: tuple) -> int:
-	count = 0
-	if row[1]:
-		if('items' in row[1]):
-			for item in row[1]['items']:
-				if('classification' in item):
-					id = item['classification']['id'][0:4]
-					if id in count_map:
-						count_map[id] += 1
-					else:
-						count_map[id] = 1
-					count += 1
-	return count
+	def process_row(self, row: tuple, colNumber: int):
+		idArr = [0] *( len(list(self.main_map_70)) + 1 )
+		if row[colNumber]:
+			if('items' in row[colNumber]):
+				for item in row[colNumber]['items']:
+					if('classification' in item):
+						id = item['classification']['id'][0:4]
+						if id in self.main_map_70:
+							index = self.main_map_70[id]
+							idArr[index] += 1
+						else:
+							idArr[-1] += 1
+						
+		return idArr
 
-def get_count_and_load_map(rows: list[tuple]) -> int:
-	total_count: int = 0
-	for row in rows:
-		total_count += load_count_map(row)
-	return total_count
+	def print_dict(self, file_name:str, dict: dict):
+		with open(f'__out/{file_name}', 'w') as f:
+			sys.stdout = f # Change the standard output to the file we created.
+			# print('This message will be written to a file.')
+			for (a, b) in dict.items():
+				print(a, ',', b)
+			sys.stdout = sys.__stdout__ # Reset
+
+	def __init__(self, rows: list[tuple], colNumber: int):
+		self.colNumber = colNumber
+		self.count_map = {}
+		self.main_map_70: dict = {}
+
+		total_count = self.get_count_and_load_map(rows)
+
+		sorted_by_count = sorted(self.count_map.items(), key=lambda x:x[1], reverse=True) #reverse True to really work
+		converted_dict = dict(sorted_by_count)
+		
+		self.load_70(converted_dict, total_count)
 	
-def load_70(counted_map: dict, total_count: int):
-	count = 0
-	index_c = 0
-	for (id, appearances_count) in counted_map.items():
-		if count <= total_count * 0.7:
-			main_map_70[id] = index_c
-			index_c += 1
-		else:
-			break
-		count += appearances_count
-
-def process_row(row: tuple):
-	idArr = [0] *( len(list(main_map_70)) + 1 )
-	if row[1]:
-		if('items' in row[1]):
-			for item in row[1]['items']:
-				if('classification' in item):
-					id = item['classification']['id'][0:4]
-					if id in main_map_70:
-						index = main_map_70[id]
-						idArr[index] += 1
-					else:
-						idArr[-1] += 1
-					
-	return idArr
-
-def print_dict(file_name:str, dict: dict):
-	with open(f'__out/{file_name}', 'w') as f:
-		sys.stdout = f # Change the standard output to the file we created.
-		# print('This message will be written to a file.')
-		for (a, b) in dict.items():
-			print(a, ',', b)
-		sys.stdout = sys.__stdout__ # Reset
-
-def main(arguments):
-	query = """
-		SELECT
-			data['compiledRelease']['ocid'] as "id",
-			data['compiledRelease']['planning'] as "planning"
-		FROM RECORD r join data d on d.id = r.data_id
-	"""
-	rows = helpers.get_rows(query)
-	total_count = get_count_and_load_map(rows)
-	print_dict('counted_map.txt', count_map)
-
-	sorted_by_count = sorted(count_map.items(), key=lambda x:x[1], reverse=True) #reverse True to really work
-	converted_dict = dict(sorted_by_count)
-	
-	print_dict('sorted_counted_map.txt', converted_dict)
-	
-	load_70(converted_dict, total_count)
-
-	print_dict('70%.txt', main_map_70)
-
-	for row in rows:
-		idArr = process_row(row)
-		print(row[0], ',', idArr)
-	
-	
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
