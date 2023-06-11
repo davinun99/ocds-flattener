39a443d93b7aad5c5002ca76323d96aa510af4c4
David <nundavid99@gmail.com>
2023-06-11 16:05:58 -0400

    covered by fixed

--- src/scripts/tender_coveredBy.py
+++ src/scripts/tender_coveredBy.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('./src')
-import helpers
+
+
 BATCH_SIZE = 1000
 
 map = {
@@ -17,22 +18,7 @@ map = {
 def process_row (row: tuple, colNumber: int) -> list[int]:
 	countArr = [0] * len(list(map))
 	if row[colNumber]:
-		if('coveredBy' in row[colNumber]):
-			for cover in row[colNumber]['coveredBy']:
-				ind = map[cover]
-				countArr[ind] += 1
+		for cover in row[colNumber]:
+			ind = map[cover]
+			countArr[ind] += 1
 	return countArr
-
-def main(arguments):
-	query = """
-		SELECT
-			data['compiledRelease']['ocid'] as "id",
-			data['compiledRelease']['tender'] as "tender"
-		FROM RECORD r join data d on d.id = r.data_id
-	"""
-	# for row in helpers.get_rows(query):
-	# 	idArr = process_row(row)
-	# 	print(idArr)
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
